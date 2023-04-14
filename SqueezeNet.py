b3df13c909894d11a622b8418f0eaac25d41e62c
Jack Kelly <58000611+JKelly423@users.noreply.github.com>
2023-04-14 15:02:44 +0200

    Main SqueezeNet file created
    This will be used to init the whole project

--- SqueezeNet.py
+++ SqueezeNet.py
@@ -1,2 +1,29 @@
-# This is my first commit because i never learned github
-# This is not my first commit because I'm a GitHub chad.
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+class SqueezeNet:
+    """The main SqeezeNet class that uses all the modules in order to perform it's task"""
+
+    # A function to perform sentiment analysis on the dataframe with a given paramaters
+    def sentiment_analysis(self, text):
+        """A function to perform sentiment analysis with vaderSentiment
+
+        :params: text: text to perform sentiment analysis on
+
+        :returns: score: the sentiment score
+        """
+
+        #Create an instance of the sentiment analyzer
+        analyzer = SentimentIntensityAnalyzer()
+
+        # Get the sentiment score
+        score = analyzer.polarity_scores(text)
+
+        # Return the sentiment score
+        return score
+
+
+
+    def __init__(self, name):
+        """The constructor of the SqueezeNet class"""
+
+        self.name = name
