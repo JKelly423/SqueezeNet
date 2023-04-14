d7151a2b6c05f86cd0a92c8485d88c1466ca77e5
Jack Kelly <58000611+JKelly423@users.noreply.github.com>
2023-04-14 15:53:55 +0200

    Commented for clarity

--- DataPlug.py
+++ DataPlug.py
@@ -5,11 +5,15 @@ import requests
 import time
 
 class DataPlug:
-    """DataPlug class. Used for data aquisition from reddit.
+    """
+    DataPlug class. Used for data aquisition from reddit.
 
     :attributes: name: name of the user agent
     :attributes: reddit_raw_data: raw data from reddit
     :attributes: df: pandas dataframe
+    :attributes: user_agent: user agent for reddit
+    :attributes: client_id: client id for reddit
+    :attributes: client_secret: client secret for reddit
     """
 
 
@@ -22,13 +26,14 @@ class DataPlug:
         :returns: data: data from reddit
         """
 
+        # Create instance of PRAW using the config file data now stored as attributes
         reddit = praw.Reddit(
             client_id=self.client_id,
             client_secret=self.client_secret,
             user_agent=self.user_agent,
         )
-        self.reddit_raw_data = reddit.subreddit(subreddit).hot(limit=limit)
 
+        self.reddit_raw_data = reddit.subreddit(subreddit).hot(limit=limit)
         return self.reddit_raw_data
 
     def data_to_pandas(self, data):
@@ -56,7 +61,7 @@ class DataPlug:
             msg = "Sorry, the file ./config.json does not exist."
             print(msg)
 
-
+        # Set the name of the net
         self.name = name
         # Set the data for the reddit instance
         self.user_agent = config['redditConfig']['user_agent']
