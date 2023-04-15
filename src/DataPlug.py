3bc1295320899ee862755ca51c091ea438e90fda
Jack Kelly <58000611+JKelly423@users.noreply.github.com>
2023-04-15 18:58:42 +0200

    DF of all reddit posts merged with price info. Looking swell so far

--- src/DataPlug.py
+++ src/DataPlug.py
@@ -109,6 +109,30 @@ class DataPlug:
         return self.df
 
 
+    def aggregate_reddit_posts_daily(self):
+
+        """Aggregate reddit posts by date. Turns dataframe for each reddit post into one dataframe with average results across all posts for each day.
+
+        :params: None
+
+        :returns: pandas dataframe
+        """
+        agg_func = {'title': list, 'score': 'mean', 'id': list, 'url': list, 'comms_num': 'mean', 'created': 'first', 'body': list}
+
+        # Replace NaN values with 'NaN'
+        self.df['body'] = self.df['body'].fillna('NaN')
+        # Group by date and aggregate
+        df_new = self.df.groupby(self.df['timestamp']).aggregate(agg_func)
+        # Join lists of titles, ids, urls, and bodies into one string per date to perform sentiment analysis.
+        df_new['title'] = df_new['title'].apply(lambda x: '||'.join(x))
+        df_new['id'] = df_new['id'].apply(lambda x: '||'.join(x))
+        df_new['url'] = df_new['url'].apply(lambda x: '||'.join(x))
+        df_new['body'] = df_new['body'].apply(lambda x: '||'.join(x))
+
+        self.df = df_new
+
+        return self.df
+
     def get_reddit_dataframe(self, filename='./../data/reddit_wsb.csv'):
         """A funciton to get dataframe of price data from csv
 
@@ -122,13 +146,17 @@ class DataPlug:
 
         return self.df
 
-    def get_price_datafrane(self, filename='./../data/GME.csv'):
+    def get_price_dataframe(self, filename='./../data/GME.csv'):
         """A funciton to get dataframe of price data from csv
 
         :params: filename: name of CSV file; defaults to './../data/GME.csv'
         :returns: pandas dataframe
         """
         self.priceDF = pd.read_csv(filename)
+        # Rename 'Date' column to 'timestamp' to match the price dataframe for merging
+        self.priceDF.rename(columns={'Date':'timestamp'}, inplace=True)
+        # Set date objects to be the same type as the reddit dataframe for merging
+        self.priceDF['timestamp'] = self.priceDF['timestamp'].astype('datetime64[ns]').dt.floor('D')
         return self.priceDF
 
     def __init__(self, name):
@@ -152,3 +180,5 @@ class DataPlug:
         self.user_agent = config['redditConfig']['user_agent']
         self.client_id = config['redditConfig']['client_id']
         self.client_secret = config['redditConfig']['client_secret']
+
+#%%
